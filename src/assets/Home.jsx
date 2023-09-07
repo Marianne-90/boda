82d59b21c3a52b455c6d2718e23f2fd266921cf5
Marianne Garrido <marianne.garridom@gmail.com>
2023-09-06 18:50:24 -0600

    Primer commit

---
+++ src/assets/Home.jsx
@@ -0,0 +1,12 @@
+export const Home = () => {
+  return (
+    <div className="Boda">
+      <div className="title">
+        <h1>Te invitamos a nuestra boda</h1>
+        <h2>Sábado 16 de diciembre</h2>
+        <h2>12:00 hrs</h2>
+        <h3>Parroquia de la Santísima Trinidad Tepango</h3>
+      </div>
+    </div>
+  );
+};
