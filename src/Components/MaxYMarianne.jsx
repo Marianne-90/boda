dcd203de75eb16906b35293e7af506555fd6a64b
Marianne Garrido <marianne.garridom@gmail.com>
2023-09-07 20:42:31 -0600

    MaxyMarianne estilos

--- src/Components/MaxYMarianne.jsx
+++ src/Components/MaxYMarianne.jsx
@@ -1,12 +1,19 @@
+import circulo from "../img/circulo.png";
+import linea from "../img/linea.png"
 export const MaxYMarianne = () => {
   return (
     <div className="maxymarianne">
-      <div className="text">
-        <p className="boda">La boda de</p>
-        <p className="name">Marianne</p>
-        <img src="" alt="" />
-        <p className="name">Maximiliano</p>
-        <p className="date">16 de diciembre, 2023</p>
+      <div className="content">
+        <div className="text">
+          <p className="boda">La boda de</p>
+          <p className="name">Marianne</p>
+          <img src={linea} alt="liena" />
+          <p className="name">Maximiliano</p>
+          <p className="date">16 de diciembre, 2023</p>
+        </div>
+        <div className="circulo">
+          <img src={circulo} alt="circulo" />
+        </div>
       </div>
     </div>
   );
